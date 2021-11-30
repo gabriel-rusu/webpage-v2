cf34d7e0af85c786198f614587f744f21e556cd7
gabriel-rusu <rusugabriel13@gmail.com>
2021-11-30 22:50:40 +0200

    updated the resume page. work on #9

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -7,6 +7,25 @@ import {Component, HostBinding, OnInit} from '@angular/core';
 })
 export class ResumeComponent implements OnInit {
   @HostBinding('attr.class') classes = 'background';
+  items: any =  {
+    "experience": [{
+      "position": "Full Stack Software Engineer",
+      "logoUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Canadian_Bank_Note_Company_logo.svg/1200px-Canadian_Bank_Note_Company_logo.svg.png"
+    }],
+    "education": [
+
+      {
+        "title": "Master's degree, Artificial Inteligence",
+        "logoUrl": "https://i.ibb.co/tzZ6xt3/download-removebg-preview-auto-x1-colored-illustration-x4-colored.png",
+        "institution": "Faculty of Automatic Control and Computers"
+      },
+      {
+        "title": "Bachelor's degree, Systems Engineering",
+        "logoUrl": "https://i.ibb.co/tzZ6xt3/download-removebg-preview-auto-x1-colored-illustration-x4-colored.png",
+        "institution": "Faculty of Automatic Control and Computers"
+      }
+    ]
+  }
 
   constructor() { }
 
